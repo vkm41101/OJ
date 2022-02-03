10daf87023159ead1817779e74207d26fb477ee8
Vivek Kumar Mishra <vkm41101@gmail.com>
2022-02-03 20:59:04 +0530

    Coode Running Working

--- Judge/app.js
+++ Judge/app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const mongoose= require('mongoose');
-const { exec } =require('child_process');
+const { execSync } =require('child_process');
 
 
 (async ()=>{
@@ -20,22 +20,81 @@ var questionTestCase = mongoose.model(
 ); 
 
 
-runSubmission= (quesID, language, containerName) =>{
+runSubmission= async (quesID, submissionID, language, containerName) =>{
     var testCases=[];
     var length=0;
-    questionTestCase.findOne({questionID: quesID}).then((data)=>{
-        if(data.toString()=='')
-        {
-            return -1;
-        }
-        testCases= data.testCases;
-        length=testCases.length;
-        for (let i = 0; i <length; i++) {
-          fs.writeFileSync('./folderrun/sampleTestCase/tc'+(i+1).toString()+'i.txt', testCases[i].input);
-          fs.writeFileSync('./folderrun/sampleTestCase/tc'+(i+1).toString()+'o.txt', testCases[i].output);
-        }
-
+    await questionTestCase.findOne({questionID: quesID}).then(async (data)=>{
+      if(data.toString()=='')
+      {
+          return -1;
+      }
+      testCases= data.testCases;
+      length=testCases.length;
+      for (let i = 0; i <length; i++) {
+        await fs.writeFileSync(__dirname + '/folderrun/sampleTestCase/tc'+(i+1).toString()+'i.txt', testCases[i].input);
+        await fs.writeFileSync(__dirname + '/folderrun/sampleTestCase/tc'+(i+1).toString()+'o.txt', testCases[i].output);
+      }
     })
+    //compille
+    var compileCommand="";
+    if(language=='C')
+    {
+      compileCommand="gcc /home/folderrun/"+ submissionID + ".c -o /home/folderrun/"+submissionID+".out";
+    }
+    else if(language=='C++')
+    {
+      compileCommand="g++ /home/folderrun/"+ submissionID + ".cpp -o /home/folderrun/"+submissionID+".out"
+    }
+    else if(language=='java')
+    {
+      compileCommand="";
+    }
+    if(language!='python3')
+    {
+      console.log('sudo docker exec ' + containerName + " " + compileCommand);
+      try
+      {
+        await execSync('sudo docker exec ' + containerName + " " + compileCommand);
+      }
+      catch
+      {
+        return -5;
+      }
+    }
+    
+    
+    //execution
+    var executeCommand = "";
+    if(language !='python3')
+    {
+      executeCommand = "/home/folderrun/"+submissionID+".out "
+    }
+    else 
+    {
+      executeCommand = "python3 /home/folderrun/" + submissionID + ".py ";
+    }
+
+    for(let i = 0; i<length; i++)
+    {
+      inputPath = "home/folderrun/sampleTestCase/tc" + (i + 1).toString() + "i.txt";
+      outputPath = 'home/folderrun/outputAnswer/tc'+(i+1).toString()+'a.txt'
+      executeCommand1 = executeCommand + " < " + inputPath + " > " + outputPath;
+      console.log("sudo docker exec -it " + containerName + ' /bin/sh -c "' + executeCommand1 + '"');
+      await execSync("sudo docker exec " + containerName + ' /bin/sh -c "' + executeCommand1 + '"', (err, stdout, stderr)=>{
+        if(stderr)
+        {
+          return -3
+        }
+        if(err)
+        {
+          return -2
+        }
+      });
+    }
+    return 0;
 }
 
-runSubmission('Q21', 'python', 'qk')
+(async ()=>{var result= await runSubmission("Q21", "test", "python3", "eager_grothendieck");
+console.log(result);})();
+
+module.exports = { runSubmission };
